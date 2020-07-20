70ac339743e71b3ba6b05efaef0719daa310721c
Rick <rick@rickkauffman.com>
2020-07-20 09:46:04 -0700

    initial release

--- main/views.py
+++ main/views.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
+# Copyright (c) 2020 Rick Kauffman
 
-# Copyright (c) 2017, Palo Alto Networks
-#
-# Permission to use, copy, modify, and/or distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE..
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # __author__ = "@netwookie"
 # __credits__ = ["Rick Kauffman"]
-# __license__ = "Apache2.0"
+# __license__ = "MIT"
 # __maintainer__ = "Rick Kauffman"
 # __email__ = "rick.a.kauffman@hpe.com"
 
@@ -45,6 +50,7 @@ from werkzeug.utils import secure_filename
 import datetime
 import random
 import sys
+# Copyright (c) 2017, Palo Alto Networks
 from pandevice import device
 from pandevice import firewall
 from pandevice import network
@@ -142,24 +148,6 @@ def deploy():
     if interface == 'none selected':
         error = "ERR2222-Go back you didn't select an interface"
         return render_template('main/gen_error.html', error=error)
-
-
-    ############################################################################
-    # Deploy code goes hereby
-    ############################################################################
-    # Copyright (c) 2017, Palo Alto Networks
-    #
-    # Permission to use, copy, modify, and/or distribute this software for any
-    # purpose with or without fee is hereby granted, provided that the above
-    # copyright notice and this permission notice appear in all copies.
-    #
-    # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-    # WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-    # MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-    # ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-    # WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-    # ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-    # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE..
 
     '''
     Handling HA
